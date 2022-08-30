7076ff2cb2d39bf1ecd562275645054ec5df6772
repyHX <90491387+repyHX@users.noreply.github.com>
2022-08-30 07:54:15 -0700

    Add files via upload

---
+++ Lesson3_project/js/main.js
@@ -0,0 +1,5 @@
+//Hernan Acosta HER2138151 8/28/22
+
+var avengers = ['Thor', 'Wasp', 'Captain America', 'Iron Man', 'Hulk', 'Vision', 'Hank Pym', 'Black Panther', 'Quicksilver', 'Mantis', 'Spiderman']
+
+document.getElementById('avengersGo').innerHTML = avengers
